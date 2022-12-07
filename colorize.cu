e2f80717f6c75929c6296b1710a26d54d1165806
BobChuckyJoe <bobchuckyjoe@gmail.com>
2022-12-07 13:25:23 -0600

    fioewjafo

--- colorize.cu
+++ colorize.cu
@@ -137,75 +137,86 @@ struct colorize_functor
 {
     __host__ __device__
         int operator()(const short& depth) const { 
-            // float disp = 1.0 / depth;
-            // float disp_max = 1.0 / DMIN;
-            // float disp_min = 1.0 / DMAX;
-            // float dnormal = (disp - disp_min) / (disp_max - disp_min);
             float d = depth;
             if (depth < DMIN) d = DMIN;
             if (depth > DMAX) d = DMAX;
+            float disp = 1.0 / d;
+            float disp_max = 1.0 / DMIN;
+            float disp_min = 1.0 / DMAX;
+            float dnormal = (disp - disp_min) / (disp_max - disp_min);
+            int d_bins = dnormal * 1529;
+            // float d = depth;
+            // if (depth < DMIN) d = DMIN;
+            // if (depth > DMAX) d = DMAX;
 
             // Gonna make it between 0-1 for now
-            float dnormal = (d - DMIN) / (DMAX - DMIN); // * 1529;
             printf("Running with depth: %d normalized to %f\n", depth, dnormal);
             
             // Convert HSV to RGB
-            hsv in = {dnormal * 360, 1, 1};
+            // hsv in = {dnormal * 360, 1, 1};
             
-            double      hh, p, q, t, ff;
-            long        i;
-            rgb         out;
+            // double      hh, p, q, t, ff;
+            // long        i;
+            // rgb         out;
 
-            if(in.s <= 0.0) {       // < is bogus, just shuts up warnings
-                out.r = in.v;
-                out.g = in.v;
-                out.b = in.v;
-                return 0;
-            }
-            hh = in.h;
-            if(hh >= 360.0) hh = 0.0;
-            hh /= 60.0;
-            i = (long)hh;
-            ff = hh - i;
-            p = in.v * (1.0 - in.s);
-            q = in.v * (1.0 - (in.s * ff));
-            t = in.v * (1.0 - (in.s * (1.0 - ff)));
+            // if(in.s <= 0.0) {       // < is bogus, just shuts up warnings
+            //     out.r = in.v;
+            //     out.g = in.v;
+            //     out.b = in.v;
+            //     return 0;
+            // }
+            // hh = in.h;
+            // if(hh >= 360.0) hh = 0.0;
+            // hh /= 60.0;
+            // i = (long)hh;
+            // ff = hh - i;
+            // p = in.v * (1.0 - in.s);
+            // q = in.v * (1.0 - (in.s * ff));
+            // t = in.v * (1.0 - (in.s * (1.0 - ff)));
 
-            switch(i) {
-            case 0:
-                out.r = in.v;
-                out.g = t;
-                out.b = p;
-                break;
-            case 1:
-                out.r = q;
-                out.g = in.v;
-                out.b = p;
-                break;
-            case 2:
-                out.r = p;
-                out.g = in.v;
-                out.b = t;
-                break;
+            // switch(i) {
+            // case 0:
+            //     out.r = in.v;
+            //     out.g = t;
+            //     out.b = p;
+            //     break;
+            // case 1:
+            //     out.r = q;
+            //     out.g = in.v;
+            //     out.b = p;
+            //     break;
+            // case 2:
+            //     out.r = p;
+            //     out.g = in.v;
+            //     out.b = t;
+            //     break;
 
-            case 3:
-                out.r = p;
-                out.g = q;
-                out.b = in.v;
-                break;
-            case 4:
-                out.r = t;
-                out.g = p;
-                out.b = in.v;
-                break;
-            case 5:
-            default:
-                out.r = in.v;
-                out.g = p;
-                out.b = q;
-                break;
-            }
-            return (int)(out.r * 255) << 16 | (int)(out.g * 255) << 8 | (int)(out.b * 255);
+            // case 3:
+            //     out.r = p;
+            //     out.g = q;
+            //     out.b = in.v;
+            //     break;
+            // case 4:
+            //     out.r = t;
+            //     out.g = p;
+            //     out.b = in.v;
+            //     break;
+            // case 5:
+            // default:
+            //     out.r = in.v;
+            //     out.g = p;
+            //     out.b = q;
+            //     break;
+            // }
+            // uint8_t derp = -1;
+            // printf("derp: %d\n", derp);
+            // uint8_t derp2 = 256;
+            // printf("derp2: %d\n", derp2);
+            // uint8_t derp3 = 256 +  256 + 12;
+            // printf("derp3: %d\n", derp3);
+            // return (int)(out.r * 255) << 16 | (int)(out.g * 255) << 8 | (int)(out.b * 255);
+            
+            // My sus implementation
             // float H = dnormal * 360;
             
             // float S = 1;
@@ -234,54 +245,55 @@ struct colorize_functor
             // return depth;
             // return to_ret;
 
-            uint8_t pr = 50;
-            // if ((0 <= dnormal && dnormal <= 255) || (1275 < dnormal && dnormal <= 1529)) {
-            //     pr = 255;
-            // } else if (255 < dnormal && dnormal <= 510) {
-            //     pr = 255 - dnormal;
-            // } else if ( 510 < dnormal && dnormal <= 1020) {
-            //     pr = 0;
-            // }
-            // else if ( 1020 < dnormal && dnormal <= 1275) {
-            //     pr = dnormal - 1020;
-            // }
-            // else {
-            //     printf("pr not set with dnormal: %f\n", dnormal);
-            // }
+            int32_t pr = 0;
+            if ((0 <= d_bins && d_bins <= 255) || (1275 < d_bins && d_bins <= 1529)) {
+                pr = 255;
+            } else if (255 < d_bins && d_bins <= 510) {
+                pr = 255 - d_bins;
+            } else if ( 510 < d_bins && d_bins<= 1020) {
+                pr = 0;
+            }
+            else if ( 1020 < d_bins && d_bins <= 1275) {
+                pr = d_bins - 1020;
+            }
+            else {
+                printf("pr not set with d_bins: %d\n", d_bins);
+            }
 
-            uint8_t pg = 50;
-            // if (0 < dnormal && dnormal <= 255) {
-            //     pg = dnormal;
-            // }
-            // else if (255 < dnormal && dnormal <= 510) {
-            //     pg = 255;
-            // }
-            // else if (510 < dnormal && dnormal <= 765) {
-            //     pg = 765 - dnormal;
-            // }
-            // else if (765 < dnormal && dnormal < 1529) {
-            //     pg = 0;
-            // }
-            // else {
-            //     printf("pg not set with dnormal: %f\n", dnormal);
-            // }
+            int32_t pg = 50;
+            if (0 <= d_bins && d_bins <= 255) {
+                pg = d_bins;
+            }
+            else if (255 < d_bins && d_bins <= 510) {
+                pg = 255;
+            }
+            else if (510 < d_bins && d_bins <= 765) {
+                pg = 765 - d_bins;
+            }
+            else if (765 < d_bins && d_bins <= 1529) {
+                pg = 0;
+            }
+            else {
+                printf("pg not set with d_bins: %d\n", d_bins);
+            }
 
-            uint8_t pb = 50;
-            // if (0 < dnormal && dnormal <= 765) {
-            //     pb = dnormal;
-            // }
-            // else if (765 < dnormal && dnormal <= 1020) {
-            //     pb = 765 - dnormal;
-            // }
-            // else if (1020 < dnormal && dnormal <= 1275) {
-            //     pb = 255;
-            // }
-            // else if (1275 < dnormal && dnormal <= 1529) {
-            //     pb = 1529 - dnormal;
-            // }
-            // else {
-            //     printf("pb not set with dnormal: %f\n", dnormal);
-            // }
+            int32_t pb = 50;
+            if (0 <= d_bins && d_bins <= 765) {
+                pb = d_bins;
+            }
+            else if (765 < d_bins && d_bins <= 1020) {
+                pb = 765 - d_bins;
+            }
+            else if (1020 < d_bins && d_bins <= 1275) {
+                pb = 255;
+            }
+            else if (1275 < d_bins && d_bins <= 1529) {
+                pb = 1529 - d_bins;
+            }
+            else {
+                printf("pb not set with d_bins: %d\n", d_bins);
+            }
+            return (pr & 0xFF) << 16 | (pg & 0xFF) << 8 | (pb & 0xFF);
             // pr = 1;
             // pg = 2;
             // pb = 3;
@@ -297,6 +309,7 @@ struct colorize_functor
             // to_ret |= 255;
             // return to_ret;
         }
+
 };
 
 
@@ -359,6 +372,33 @@ uint16_t to_depth(int rgb_int) {
     return DMIN + (DMAX - DMIN) * H_norm;
 }
 
+uint16_t to_depth_paper(int rgb_int) {
+    int r = (rgb_int >> 16) & 0xFF;
+    int g = (rgb_int >> 8) & 0xFF;
+    int b = rgb_int & 0xFF;
+    int drnormal;
+    if (r >= g && r >= b && g >= b) {
+        drnormal = g - b;
+    }
+    else if (r >= g && r >= b && g < b) {
+        drnormal = g - b + 1529;
+    }
+    else if (g >= r && g >= b) {
+        drnormal = b - r + 510;
+    }
+    else if (b >= g && b >= r){
+        drnormal = r - g + 1020;
+    }
+    else {
+        printf("OIWEJFOIJWEOFIEJWOJ Bad color: %d, %d, %d", r, g, b);
+    }
+    float disp_min = 1/DMAX;
+    float disp_max = 1/DMIN;
+    float recovered_depth = 1529.0 / (1529.0 * disp_min + (disp_max - disp_min) * drnormal);
+
+    return recovered_depth;
+}
+
 uint8_t* exported_colorize(uint8_t* depth_buf, uint32_t depth_buf_len) {
     return (uint8_t*)colorize((uint16_t*)depth_buf, depth_buf_len / 2);
 }
@@ -379,7 +419,7 @@ int main() {
     }
     // Convert back to depth
     for (int i = 0; i < 8; i++) {
-        std::cout << to_depth(output[i]) << std::endl;
+        std::cout << to_depth_paper(output[i]) << std::endl;
     }
     return 0;
 }
